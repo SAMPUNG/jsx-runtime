52c43ceb8354c10a536e76fde2ebd47ba2a0b85f
Maquia <illhyearn@163.com>
2024-05-10 14:24:22 +0800

    🔊 Update Type File

--- types/index.d.ts
+++ types/index.d.ts
@@ -10,3 +10,13 @@ export declare function fragment(
   _attrs?: { [key: string]: any },
   ...children: (HTMLElement | string)[]
 ): (HTMLElement | string)[]
+
+export declare function clear(elm: HTMLElement): void
+
+export declare function createSignal<T>(value: T): {
+  get: (template?: string) => any;
+  refer: (elm: HTMLElement, event: string) => void;
+  relay: (elm: HTMLElement, prop: string, template?: string) => void;
+  set: (next: T) => void;
+  sync: (elm: HTMLElement) => boolean;
+}
